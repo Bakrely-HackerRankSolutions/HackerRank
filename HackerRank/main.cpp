32e6d61d60dadae94fb1335de69286b6fab98dbc
Bakr <ab.elyounoussi@gmail.com>
2020-09-24 14:07:13 +0200

    Initial Commit

---
+++ HackerRank/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HackerRank
+//
+//  Created by xadmin on 24.09.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
